aeeb967831fc7e4f247c15654f526430441fed4e
DivaStress <divayudha4@gmail.com>
2023-08-22 13:59:02 +0700

    add objectComplex repo

---
+++ 12-objectComplex/text.js
@@ -0,0 +1,27 @@
+var myAnime = [
+    {
+        "Anime" :   "Konosuba",
+        "Genre" :   "Comedy",
+        "realease_year" :   "2014",
+        "characters"    :   [
+            "Kazuma",
+            "Aqua",
+            "Megumin",
+            "Darkness"
+        ],
+        "party" :   true,
+    },
+    {
+        "Anime" :   "Umaru",
+        "Genre" :   "Comedy",
+        "realease_year" :   "2012",
+        "characters"    :   [
+            "Umaru",
+            "Onii-chan",
+        ],
+        "huh?" :   true,
+    }
+]
+
+var charaName = myAnime[0].characters[2];
+console.log(charaName);

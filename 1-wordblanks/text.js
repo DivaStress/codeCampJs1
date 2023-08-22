ad94e9fb02781639d89c36fec62a6bd25ca91df9
DivaStress <divayudha4@gmail.com>
2023-08-22 07:27:58 +0700

    initial

---
+++ 1-wordblanks/text.js
@@ -0,0 +1,8 @@
+function wordBlanks(myNoun,myAdjective,myVerb,myAdverb){
+    var result = "";
+    result += myNoun + " wanna" + myAdjective + "my" + myNoun +myVerb + "in the hole";
+
+    return result;
+}
+
+console.log(wordBlanks("I" , "fuck" , "Waifu" , "quickly"));

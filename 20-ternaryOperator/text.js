9aaf3e55c1bed30428d37d869fb89da040d63670
DivaStress <divayudha4@gmail.com>
2023-08-23 08:05:56 +0700

    add ternaryOperator repo

---
+++ 20-ternaryOperator/text.js
@@ -0,0 +1,9 @@
+function checkEqual(a,b){
+    return a === b ? true : false;
+    }
+console.log(checkEqual(2,2));
+
+function checkSign(num){
+    return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
+}
+console.log(checkSign(0))

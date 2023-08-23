5c311206f8f5dcec950b13ec2ddef6d255d365d0
DivaStress <divayudha4@gmail.com>
2023-08-23 08:46:27 +0700

    add varAndLet

---
+++ 21-varAndLet/text.js
@@ -0,0 +1,14 @@
+let catName = "nyanko";
+let quote;
+
+catName = "Neko";
+
+function catTalk(catName,quote){
+
+    catName = "Kyo";
+    quote = catName + " Nyaa";
+
+    return quote;
+    }
+
+console.log(catTalk());

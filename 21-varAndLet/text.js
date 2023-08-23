99984b19dfe13d9204c9ead9d399d92eea1d03d3
DivaStress <divayudha4@gmail.com>
2023-08-23 10:28:38 +0700

    update code

--- 21-varAndLet/text.js
+++ 21-varAndLet/text.js
@@ -12,3 +12,14 @@ function catTalk(catName,quote){
     }
 
 console.log(catTalk());
+
+function checkScope(){
+    let i = "function scope";
+    if(true){
+        let i = "block scope";
+        console.log("block scope i is", i);
+    }
+    console.log("function scope i is", i);
+}
+
+checkScope();
